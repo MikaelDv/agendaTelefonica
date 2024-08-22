8e3c2f526b823c787fce0106d37d1f6783269a4a
MikaelDv <enzomikael@hotmail.com>
2024-08-22 12:27:27 -0300

    Conexão com o banco de dados oracle, utilizando variaveis de ambiente

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -1,4 +1,20 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    private static String user = System.getenv("FIAP_ORACLE_USER");
+    private static String password = System.getenv("FIAP_ORACLE_PASSWORD");
+
+    public static Connection obterConnection() {
+        Connection conexao = null;
+        try {
+            conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", user, password);
+        } catch (SQLException erro) {
+            erro.printStackTrace();
+        }
+        return conexao;
+    }
 }
